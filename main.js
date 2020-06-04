e4e7c1458000e0f77698143fde8312100d7be6c6
DavidCastillo4 <David.Castillo4@gmail.com>
2020-06-04 12:19:09 -0500

    reset

--- main.js
+++ main.js
@@ -12,33 +12,10 @@ const rl = readline.createInterface({
 });
 
 // the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
-  let Words = ['rock', 'paper', 'scissors'];
-  let i = Words.indexOf(hand1.toLowerCase());
-  let ii = Words.indexOf(hand2.toLowerCase());
-  let SumN = i + ii;
-  let Outcome='Hand two wins!'
-  , Winner = 'Hand one wins!'
-  , Tie = "It's a tie!";
 
-  if (i == ii) {
-      //Msg = 'Tie';
-      Outcome = Tie;
-  }
-  else if (SumN == 1) {
-      //Msg = 'Paper covers Rock';
-      if (i == 1) Outcome = Winner;
-  }
-  else if (SumN == 2) {
-      //Msg = 'Rock destroys Scissors';
-      if (i == 0) Outcome = Winner;
-  }
-  else if (SumN == 3) {
-      //Msg = 'Scissors cut Paper';
-      if (i == 2) Outcome = Winner;
-  };  
-  console.log(Outcome)
-}
+
+
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
