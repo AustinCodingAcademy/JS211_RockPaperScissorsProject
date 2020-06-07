f5cfd4054e8ad68b06c1801d135626ec627b0d79
Apple <apple@Fideles-Mac-Pro.attlocal.net>
2020-06-07 13:09:01 -0500

    initial commit rps

--- main.js
+++ main.js
@@ -1,62 +1,29 @@
 // uses strict mode so strings are not coerced, variables are not hoisted, etc... 
 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var hand1 = prompt("What is your choice ?");
+var hand2 = prompt("What about yours ?");
 
-// the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
-
-  // Write code here
-  // Use the unit test to see what is expected
-
+if(hand1 == "" || hand2 =="") {
+ console.log("Input 1 is empty please try again")
+}
+else if(hand2 == ""){
+ console.log("Input 2 is empty please try again") 
+}    
+else if(hand1 === hand2 && hand1,hand2 ==['rock','paper','scissors']) {
+  console.log ("It is a tie");
+}
+else if(hand1 === "rock" && hand2 === "paper"||hand1 ==="paper" && hand2 === "scissors"){
+      console.log("hand2 wins");
+}
+else if(hand1 === "rock" && hand2 === "scissors") {
+  console.log("hand1 wins")
+}
+else{
+  console.log('try again')
+ 
 }
 
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
-    });
-  });
-}
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
 
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built return the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
 
-  // always returns ask the user for another input
-  getPrompt();
-
-}
