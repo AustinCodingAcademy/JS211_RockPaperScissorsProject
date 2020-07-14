b78a7723934444b5809bc9285d9a6b5fa9cdeecb
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-07-14 16:59:05 -0500

    commented out stuff

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ let value1 = ""
 let value2 = ""
 
 // brings in the assert module for unit testing
-// const assert = require('assert');
+const assert = require('assert');
 // brings in the readline module to access the command line
-// const readline = require('readline');
+const readline = require('readline');
 // use the readline module to print out to the command line
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // the function that will be called by the unit test below
 
